08232a3a2e5451f5c31234dc62074b6a2bda763f
gabbkate <ad@xakep.pw>
2017-05-28 00:12:47 +0500

    Update button.js

--- js/button.js
+++ js/button.js
@@ -9,8 +9,12 @@ $('.dropdown-button').dropdown({
       stopPropagation: false // Stops event propagation
     }
   );
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
 
- $('.modal').modal({
+$('.modal').modal({
       dismissible: true, // Modal can be dismissed by clicking outside of the modal
       opacity: .5, // Opacity of modal background
       inDuration: 300, // Transition in duration
